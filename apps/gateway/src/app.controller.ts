9e0c7de99d6492d5f01194766eb4fff03d1ced56
Alireza <alirezashadow52555@gmail.com>
2024-03-13 22:35:22 +0330

    back update

--- apps/gateway/src/app.controller.ts
+++ apps/gateway/src/app.controller.ts
@@ -12,11 +12,7 @@ export class AppController {
   async getTokenPrice(@Query() query: { addressOne: string, addressTwo: string ,chainId: string}): Promise<any> {
     console.log("API called. fetching...");
     
-    // Moralis.start({
-    //   apiKey: "TsLS6Uwt4tfB5QoDEmhh82BVsIRK7zqdwPI9LmTyUmt4aLend9KxfZWfpfTc7iEF"
-    //   // apiKey: process.env.MORALIS_KEY,
-    //   // apiKey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6Ijk4NGFmNjBjLTlhNWQtNDkxYi1hYWUyLTc2ODcyMDM2ZDRmOSIsIm9yZ0lkIjoiMjY4MzY2IiwidXNlcklkIjoiMjczMTUwIiwidHlwZUlkIjoiYmE0NmRjZTEtYTE3Ni00NTgwLTliZGMtZjM0MGJjYTk4ZDFlIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MDc2NzgzMDksImV4cCI6NDg2MzQzODMwOX0.Lwf22ngYdnKoCSE1J-WCUrstQXUvtnLiY4MrZ6ST1qI"
-    // });
+
 
     const responseOne = await Moralis.EvmApi.token.getTokenPrice({
       address: query.addressOne,chain: query.chainId
