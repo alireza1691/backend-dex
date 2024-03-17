38053be401135b748f431aec5989e65ab3da2a70
Alireza <alirezashadow52555@gmail.com>
2024-03-17 19:48:14 +0330

    Quote

--- apps/blockchain/src/blockchain.service.ts
+++ apps/blockchain/src/blockchain.service.ts
@@ -11,16 +11,19 @@ export class BlockchainService {
     // const apiKey =process.env.ONEINCH_API_KEY
     const apiKey = "GWUfUd03Z8rEwi83iStpmPgtkiLBm4S4"
     console.log("api key:",apiKey);
+    console.log(process.env.ONEINCH_API_KEY);
+    
     const requestData = {src:src,dst:dst,amount:amount}
     const headers = {'Content-Type':"application/json",
   'Api-Key':apiKey}
     
-    const url = `https://api.1inch.io/v6.0/1/quote?src=${src}&dst=${dst}&amount=${amount}`;
-    const pureUrl = `https://api.1inch.io/v6.0/1/quote`;
+    const url = `https://api.1inch.io/v5.0/1/quote?src=${src}&dst=${dst}&amount=${amount}`;
+    const pureUrl = `https://api.1inch.io/v5.0/1/quote`;
+    const allowanceUrl = `https://api.1inch.io/v6.0/1/approve/allowance?tokenAddress=0x111111111117dc0aa78b770fa6a738034120c302&walletAddress=0x39A77B13BA2C5FA2249f7e5a4194582824D58c8E`
     try {
-      // const response = await axios.get(url);
-      const response = await axios.get(url,{headers});
-      console.log(response.data);
+      const response = await axios.get(allowanceUrl);
+      // const response = await axios.get(url,{headers});
+      console.log(response);
       
       return response.data;
     } catch (error) {
